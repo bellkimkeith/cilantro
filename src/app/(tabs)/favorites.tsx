22aa3ccf966ffcaed445c0df9b51aecfdedf38dd
bellkimkeith <157334752+bellkimkeith@users.noreply.github.com>
2024-05-13 22:14:51 +0800

    backgrounds for empty pages

--- src/app/(tabs)/favorites.tsx
+++ src/app/(tabs)/favorites.tsx
@@ -36,5 +36,6 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    backgroundColor: "#E0FBE2",
   },
 });
--- src/app/(tabs)/groceries.tsx
+++ src/app/(tabs)/groceries.tsx
@@ -112,5 +112,6 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    backgroundColor: "#E0FBE2",
   },
 });
